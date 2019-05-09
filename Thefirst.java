6e53d577267c46968e3803e77f942977d335a974
xiwuzr <972490350@qq.com>
2019-05-09 21:26:01 +0800

    firsy day

---
+++ Thefirst.java
@@ -0,0 +1,16 @@
+public class Thefirst
+{
+	public static void main(String[] args)
+	{
+		char aChar='a';
+		char enterChar='\r';
+		char ch='\u9999';
+		System.out.println(ch);
+		char zhong='ท่';
+		System.out.println(zhong);
+		int zhongV=zhong;
+		System.out.println(zhongV);
+		char c=97;
+		System.out.println(enterChar);
+	}
+}
